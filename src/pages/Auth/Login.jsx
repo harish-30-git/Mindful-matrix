5879645037bb289932cf981ea402492cf9d086de
harish-30-git <srihariprakash1112@gmail.com>
2025-09-09 09:51:34 +0530

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/login", {
+      const response = await fetch("https://mindmatrix-3.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
